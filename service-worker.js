acc15ab6bb178c42bd28542ffa5e52aadfef9a6e
emilwallnoefer <emil.wallnoefer@gmail.com>
2025-10-01 02:03:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE='hour-logger-v17';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = 'hour-logger-v15';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'
+];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
