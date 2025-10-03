74cd40cbaff489301e1b6d607879e3929c9ac850
emilwallnoefer <emil.wallnoefer@gmail.com>
2025-10-04 00:21:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='hour-logger-28';
+const CACHE='hour-logger-23';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))) });
 self.addEventListener('activate',e=>{
